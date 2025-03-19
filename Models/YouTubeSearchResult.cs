b5eeaaf18eb343b54c3526f486f38ee5b6ed867c
Ceed03 <mrmuqsit2003@gmail.com>
2025-03-19 16:13:20 +0800

    Fix Syntax

--- Models/YouTubeSearchResult.cs
+++ Models/YouTubeSearchResult.cs
@@ -2,7 +2,7 @@
 {
     public class YouTubeSearchResult
     {
-        public List<YouTubeVideo> Videos { get; set; } = new();
+        public required  List<YouTubeVideo> Videos { get; set; } = new();
         public string? NextPageToken { get; set; }
         public string? PrevPageToken { get; set; }
         public int CurrentPage { get; set; } = 1; // Default to page 1
--- Models/YouTubeVideo.cs
+++ Models/YouTubeVideo.cs
@@ -1,11 +1,8 @@
-﻿namespace YouBrowse.Models
+﻿public class YouTubeVideo
 {
-    public class YouTubeVideo
-    {
-        public string VideoId { get; set; }
-        public string Title { get; set; }
-        public string ChannelName { get; set; }
-        public string PublishedAt { get; set; }
-        public string ThumbnailUrl { get; set; }
-    }
+    public required string VideoId { get; set; }
+    public required string Title { get; set; }
+    public required string ChannelName { get; set; }
+    public required string PublishedAt { get; set; }
+    public required string ThumbnailUrl { get; set; }
 }
